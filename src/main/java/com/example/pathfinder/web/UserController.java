04d8bc7e4ffecb18b8d0f2dd248761de5e582503
Teodora Uzunova <tedyuzunova@gmail.com>
2022-05-13 16:40:04 +0300

    add home and user controllers

--- src/main/java/com/example/pathfinder/web/UserController.java
+++ src/main/java/com/example/pathfinder/web/UserController.java
@@ -1,21 +1,42 @@
 package com.example.pathfinder.web;
 
+import com.example.pathfinder.model.binding.UserRegisterBindingModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+    @ModelAttribute
+    public UserRegisterBindingModel userRegisterBindingModel() {
+        return new UserRegisterBindingModel();
+    }
+
     @GetMapping("/register")
-    public String register() {
+    public String register(Model model) {
         return "register";
     }
 
     @PostMapping("/register")
-    public String registerConfirm() {
+    public String registerConfirm(@Valid UserRegisterBindingModel userRegisterBindingModel,
+                                  BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes
+                    .addFlashAttribute("userRegisterBindingModel", userRegisterBindingModel)
+                    .addFlashAttribute("org.springframework.validation.BindingResult.userRegisterBindingModel", bindingResult);
+
+            return "redirect:register";
+        }
         // TODO
 
         return "redirect:/";
